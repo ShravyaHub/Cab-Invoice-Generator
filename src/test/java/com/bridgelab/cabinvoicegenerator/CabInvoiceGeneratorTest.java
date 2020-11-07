d4c81ea0ca5963ba1850d087337cfdef38d9ef19
ShravyaHub <shravyak710@gmail.com>
2020-11-07 19:22:12 +0530

    ADDED: code to generate invoice which had total number of rides, ttal fare and averafe fare per ride

--- src/test/java/com/bridgelab/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelab/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -6,15 +6,33 @@ import org.junit.Test;
 public class CabInvoiceGeneratorTest {
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        int distance = 10, time = 12;
-        Assert.assertEquals(112, new CabInvoiceGenerator().calculateFare(distance, time));
+    public void givenDistanceAndTime_IfFareGreaterThanMinimumFare_ShouldReturnTotalFare() {
+        double distance = 10;
+        int time = 12;
+        Assert.assertEquals(112, new CabInvoiceGenerator().calculateFare(distance, time), 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_IfFareLessThanMinimumFare_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        Assert.assertEquals(5, new CabInvoiceGenerator().calculateFare(distance, time), 0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
         Ride[] rides = { new Ride(10, 15), new Ride(3, 8), new Ride(4, 10) };
-        Assert.assertEquals(203, new CabInvoiceGenerator().calculateTotalAggregateFare(rides));
+        Assert.assertEquals(203, new CabInvoiceGenerator().calculateTotalAggregateFare(rides), 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {
+                new Ride(7, 15),
+                new Ride(2.3, 8),
+                new Ride(0.8, 4)
+        };
+        Assert.assertEquals(new InvoiceSummary(3,  128), new CabInvoiceGenerator().getInvoiceSummary(rides));
     }
 
 }
