2fac8d7a91e8fd6dd5ab8488d196b371ab4c70b6
ShravyaHub <shravyak710@gmail.com>
2020-11-07 18:32:47 +0530

    ADDED: given distance and time, the invoice generator should return the total fare for the journey

---
+++ src/test/java/com/bridgelab/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,16 @@
+package com.bridgelab.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        int distance = 10, time = 12;
+        int totalFare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(112, totalFare);
+    }
+
+}
